9a4ae86495f02b66d69538b659117bbba04f2f9f
marco-cristofano <marco.cristofano.dev@gmail.com>
2026-03-14 17:56:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 nombre = "Juan"
 apellido = "Castaño"
 edad = "21"
+pais = "Argentina"
 
 print(f"Mi nombre es {nombre}")
 print(f"Mi apellido es {apellido}")
 print(f"Mi edad es {edad}")
+print(f"Mi pais es {argentina}")
